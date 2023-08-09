87f0c2a5cbcdac759d2755725ddf314d2237724d
Sanjeev-kumar001 <118330918+Sanjeev-kumar001@users.noreply.github.com>
2023-08-10 05:22:21 +0530

    Update settings.py

--- celeryproject/celeryproject/settings.py
+++ celeryproject/celeryproject/settings.py
@@ -143,7 +143,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='sahotasanjeev222@gmail.com'
-EMAIL_HOST_PASSWORD = "wbawaxywzhlycfj"
+EMAIL_HOST_PASSWORD = "wbawaxywzhlj"
 DEFAULT_FROM_EMAIL = 'Celery_integration<sahotasanjeev222@gmail.com>'
 
 
